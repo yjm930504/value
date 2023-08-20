92440b9885e757575d93845c36101f66a4fb6415
jiaming <jiaming.yan@qq.com>
2023-08-20 14:39:28 +0800

    注释

--- value/src/main/java/org/yjm/math/matrixutilities/internal/Address.java
+++ value/src/main/java/org/yjm/math/matrixutilities/internal/Address.java
@@ -5,13 +5,35 @@ import java.util.Set;
 
 public interface Address {
 
+    /**
+     * @Author  Jiaming Yan
+     * @Description 矩阵和数组的访问方法接口
+     */
+
     public static final String INVALID_BACKWARD_INDEXING = "invalid backward indexing";
     public static final String INVALID_ROW_INDEX = "invalid row index";
     public static final String INVALID_COLUMN_INDEX = "invalid column index";
     public static final String GAP_INDEX_FOUND = "gap index found";
+
+    /**
+     * @Author  Jiaming Yan
+     * @Description 行数
+     */
     public int rows();
+    /**
+     * @Author  Jiaming Yan
+     * @Description 列数
+     */
     public int cols();
+    /**
+     * @Author  Jiaming Yan
+     * @Description 基址
+     */
     public int base();
+    /**
+     * @Author  Jiaming Yan
+     * @Description 最后地址
+     */
     public int last();
     /**
      * @Author  Jiaming Yan
@@ -23,6 +45,11 @@ public interface Address {
      * @Description 矩阵列的偏移量
      */
     public int col0();
+
+    /**
+     * @Author  Jiaming Yan
+     * @Description 底层内存存储是否可以连续访问
+     */
     public boolean isContiguous();
     public boolean isFortran();
     public Set<Address.Flags> flags();
